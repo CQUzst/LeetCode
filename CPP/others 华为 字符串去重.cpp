c11bb462cc0203b2d5cd04645ca57c3c1e92b338
zst <821988519@qq.com>
2019-02-03 10:27:15 +0800

    Update and rename 字符串去重（华为）.cpp to others 华为 字符串去重.cpp

--- CPP/others 华为 字符串去重.cpp
+++ CPP/others 华为 字符串去重.cpp
@@ -5,19 +5,21 @@ int main(){
     string s;
     //dBowNgxyPZXZXlBTMKFskExQEK
     while(cin>>s){
-        int a[126]={0};
+        int a[128]={0};
         string res("");
         for(int i=0;i<s.size();i++) {
-            if(a[s[i]]>=1) continue;
+            if(a[s[i]]>=1) 
+                continue;
             a[s[i]]++;
         }
         for(int i=0;i<s.size();i++){
-            if(--a[s[i]]==0) res+=s[i];
+            if(--a[s[i]]==0) 
+                res+=s[i];
         }
         cout<<res<<endl;
     }
 }
-////¹þÏ£±í
+
 //#include<iostream>
 //#include<string>
 //#include<map>
