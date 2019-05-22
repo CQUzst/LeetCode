73ab9f1a09bec15333f7f6542a2ff35e4d4e0039
zst <821988519@qq.com>
2019-05-22 17:16:09 +0800

    Create others VIVO 最长公共子串.cpp

---
+++ CPP/others VIVO 最长公共子串.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+#define maxSize 100
+int dp[maxSize][maxSize]={0};
+
+int maxSubLength(string s1,string s2)
+{
+    //方便操作
+    int maxLength=-1,start=0;
+    for(int i=0;i<s1.length();i++){
+        for(int j=0;j<s2.length();j++){
+            if(s1[i]==s2[j]){
+                dp[i][j]=dp[i-1][j-1]+1;
+                if(maxLength<dp[i][j]){
+                    maxLength=dp[i][j];
+                    start=i;a
+                }
+            }
+            else
+                dp[i][j]=0;
+        }
+    }
+    string res=s1.substr(start-maxLength+1,maxLength);
+    cout<<res<<endl;
+    return maxLength;
+}
+
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<maxSubLength(s1,s2)<<endl;
+
+    return 0;
+}
